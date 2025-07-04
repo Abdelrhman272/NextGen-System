a8c1e2dfa691735628ebc068dcc4f348ff15950e
Abdelrhman Elsayed <info@nextgensystems.net>
2025-07-04 16:07:44 +0300

    Final fix for view types

--- fish_farm_management/__manifest__.py
+++ fish_farm_management/__manifest__.py
@@ -20,7 +20,7 @@
         'account',
         #'analytic',
         'stock',
-        'stock_picking_request'
+        'stock_picking_request',
         'sale_management',
         'purchase',
         'mrp',
